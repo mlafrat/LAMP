ba708e84fb05eb76eba2c0c911e5b4d49cb4667c
mlafrat <146100054+mlafrat@users.noreply.github.com>
2023-10-02 15:55:08 -0400

    added styling components

--- Front End LAMP/updated_php/showContacts.php
+++ Front End LAMP/updated_php/showContacts.php
@@ -15,27 +15,43 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Show All Contacts</title>
-</head>
-<body>
-  <table>
-    <tr>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Phone Number</th>
-      <th>E-mail</th>
-    </tr>
-    <?php while($rows=$result->fetch_assoc()) { ?>
-    <tr>
-      <td><?php echo $rows['contactFirst'];?></td>
-      <td><?php echo $rows['contactLast'];?></td>
-      <td><?php echo $rows['phone'];?></td>
-      <td><?php echo $rows['email'];?></td>
-    </tr>
-    <?php } ?>
-  </table>
-</body>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Contact List</title>
+    <link rel="stylesheet" href="/css/mainPage.css"/>
+    <link rel="stylesheet" href="/css/login.css" />
+    <link rel="stylesheet" href="globals.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Ubuntu"
+    />
+  </head>
+  <body>
+    <div class="showContactTitle">My Contacts</div>
+    <a href="main.php">
+      <button class="goHome">Home</button>
+    </a>
+    <table class="contactListTitles">
+      <tr>
+        <th>| First Name |</th>
+        <th>Last Name |</th>
+        <th>Phone Number |</th>
+        <th>E-mail |</th>
+      </tr>
+      <?php while($rows=$result->fetch_assoc()) { ?>
+      <tr>
+        <td><?php echo $rows['contactFirst'];?></td>
+        <td><?php echo $rows['contactLast'];?></td>
+        <td ><?php echo $rows['phone'];?></td>
+        <td><?php echo $rows['email'];?></td>
+        <td>
+          <a href="update.php">
+           <button class="updateButton">Update</button>
+          </a>
+        </td>
+      </tr>
+      <?php } ?>
+    </table>
+  </body>
 </html>
