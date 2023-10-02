4e6269753f591f4dcfa96f2a98fd1008f6290e87
Andrew Lee <andrewleesrq@gmail.com>
2023-10-02 10:57:25 -0400

    Fixed create new contact function

--- Front End LAMP/updated_php/create.php
+++ Front End LAMP/updated_php/create.php
@@ -6,11 +6,18 @@ session_start();
 	{
     $contactFirst = $_POST['contactFirst'];
 		$contactLast = $_POST['contactLast'];
+    $contactPhone = $_POST['contactPhone'];
     $contactMail = $_POST['contactMail'];
-		$contactPhone = $_POST['contactPhone'];
+    $userId = $_SESSION['user_id'];
     $conn = connectToDatabase();
-    $query = "INSERT INTO ContactInfo VALUES ('$contactName', '$contactPhone', '$contactMail', '$userId')";
+    $query = "INSERT INTO ContactInfo (contactFirst, contactLast, phone, email, userID)
+              VALUES ('$contactFirst', '$contactLast', '$contactPhone', '$contactMail', '$userId')";
     mysqli_query($conn, $query);
+
+    if (!$conn)
+    {
+      die("Failed to connect to db!".mysqli_connect_error());
+    }
   }
 
 ?>
@@ -36,7 +43,7 @@ session_start();
         <div class="frontBox"></div>
         <div class="createTitle">Create Contact</div>
         <div class="signUpBelow">Please fill all boxes below.</div>
-        <form action="" method="POST">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
           <input type="text" class="boxOne" placeholder="First Name" name="contactFirst" required />
           <br />
           <input type="text" class="boxTwo" placeholder="Last Name" name="contactLast" required />
