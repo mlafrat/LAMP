9039c65e1921bce7c4822f5ffaf8aff941e616a4
nickllages <52086760+nickllages@users.noreply.github.com>
2023-10-03 13:35:04 -0400

    Can now delete without needing to fill out inputs

--- Front End LAMP/updated_php/update.php
+++ Front End LAMP/updated_php/update.php
@@ -1,3 +1,38 @@
+<?php
+session_start();
+  require_once('includes/functions.inc.php');
+  require_once('includes/contacts.inc.php');
+  $userID = $_SESSION['user_id'];
+  if(!isset($_SESSION['contactID']))
+  {
+    $_SESSION['contactID'] = $_GET['contact_id'];
+  }
+
+  debug_to_console("$contactID");
+  if ($_SERVER["REQUEST_METHOD"] == "POST") 
+	{
+    $_SESSION['error'] = 0;
+    $contactID = $_SESSION['contactID'];
+    $contactFirst = $_POST['contactFirst'];
+		$contactLast = $_POST['contactLast'];
+    $contactPhone = $_POST['contactPhone'];
+    $contactMail = $_POST['contactMail'];
+    $conn = connectToDatabase();
+    if (!$conn){die("Failed to connect to db!".mysqli_connect_error());}
+
+    $contact = update_contact($contactID, $contactFirst,$contactLast, $contactPhone, $contactMail);
+
+    switch($contact)
+    {
+      case 1:
+        $_SESSION['error'] = 1;
+        header("Location: showContacts.php?error");
+        exit();
+    }
+    header("Location: showContacts.php");
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -20,27 +55,23 @@
         <div class="frontBox"></div>
         <div class="createTitle">Update Contact</div>
         <div class="signUpBelow">Please fill all boxes below.</div>
-        <form action="placeholder.php" method="POST">
-          <input type="text" class="boxOne" placeholder="First Name" required />
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+          <input type="text" class="boxOne" placeholder="First Name" name="contactFirst" required />
           <br />
-          <input type="text" class="boxTwo" placeholder="Last Name" required />
+          <input type="text" class="boxTwo" placeholder="Last Name" name="contactLast" required />
           <br />
-          <input type="text" class="boxThree" placeholder="Email" required />
+          <input type="text" class="boxThree" placeholder="Email" name="contactMail" required />
           <br />
-          <input
-            type="tel"
-            class="boxFour"
-            placeholder="Phone: 123-456-7890"
-            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-            required
-          />
+          <input type="tel" class="boxFour" placeholder="Phone: 123-456-7890" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="contactPhone" required />
+          <br />
           <br />
           <input type="submit" class="signUpButton" value="Update!" />
-          <a href="showContacts.php">
-            <span title="Show all contacts" class="retToMain">Click here to return to contact list!</span>
-            </a>
-          <div class="retUserText">Click below to delete contact.</div>
-        <a href="login.html">
+        </form>
+        <a href="showContacts.php">
+          <span title="Show all contacts" class="retToMain">Click here to return to contact list!</span>
+        </a>
+        <div class="retUserText">Click below to delete contact.</div>
+        <a href="delete.php">
           <button class="deleteButton">Delete</button>
         </a>
       </div>
