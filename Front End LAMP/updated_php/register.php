dc14bc1b92ec9c385762c93cc0998af20d0e9ae1
Andrew Lee <andrewleesrq@gmail.com>
2023-10-07 22:01:47 -0400

    Updated register.php to currently uploaded file

--- Front End LAMP/updated_php/register.php
+++ Front End LAMP/updated_php/register.php
@@ -1,23 +1,36 @@
 <?php
 session_start();
 	require_once('includes/functions.inc.php');
-	debug_to_console("Test");
 	if ($_SERVER["REQUEST_METHOD"] == "POST") 
 	{
+    $_SESSION['error'] = 0;
+
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
 		$userName = $_POST['userName'];
 		$password = $_POST['password'];
-		$query = "INSERT INTO Users (firstName, lastName, userName, password) Values ('$firstName','$lastName','$userName','$password')";
+		
 		$conn = connectToDatabase();
-    mysqli_query($conn, $query);
-		header("Location: login.php");
-
-    if (!$conn)
+    if(!$conn){die("Failed to connect to db!".mysqli_connect_error());}
+    
+    $register = register_user($firstName,$lastName,$userName,$password);
+    switch($register)
     {
-      die("Failed to connect to db!".mysqli_connect_error());
+      case 2:
+        $_SESSION['error'] = 2;
+        header("Location: register.php");
+        exit();
+        break;
+      case 3:
+        $_SESSION['error'] = 3;
+        header("Location: register.php");
+        exit();
+        break;
     }
-	}
+
+    header("Location: login.php");
+    exit();
+  }
 ?>
 
 <!DOCTYPE html>
@@ -38,10 +51,10 @@ session_start();
     <div class="containerWrapper">
       <div class="container">
         <div class="backBox"></div>
-        <div class="frontBox">
-          <div class="registerTitle">Sign Up</div>
-          <div class="signUpBelow">New User? Sign up below!</div>
-          <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+        <div class="frontBox"></div>
+        <div class="registerTitle">Sign Up</div>
+        <div class="signUpBelow">New User? Sign up below!</div>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
           <input type="text" class="boxOne" placeholder="First Name" name="firstName"required />
           <br />
           <input type="text" class="boxTwo" placeholder="Last Name" name="lastName"required />
@@ -50,13 +63,23 @@ session_start();
           <br />
           <input type="password" class="boxFour" placeholder="Password" name="password"required/>
           <br />
+          
+          <?php
+          if ($_SESSION['error'] == 2)
+          {
+          echo "<div class='errorRegister'>Username is either taken or does not fit the requirements.</div>";
+          }
+          if ($_SESSION['error'] == 3)
+          {
+          echo "<div class='errorRegister'>Password does not fit the requirements.</div>";
+          }
+        ?>
           <input type="submit" class="signUpButton" value="Register" />
-          </form>
-          <div class="retUserText">Existing user? Sign in below!</div>
-          <a href="login.php">
+        </form>
+        <div class="retUserText">Existing user? Sign in below!</div>
+        <a href="login.php">
           <button class="signInButton">Login</button>
-          </a>
-        </div>
+        </a>
       </div>
     </div>
   </body>
